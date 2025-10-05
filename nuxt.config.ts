4989d5f770fe2b73d479d53e7e8cda59647acc65
tkssoul <2495830486@qq.com>
2025-10-06 02:51:59 +0800

    refactor: 移除ChatbotPage相关路径配置
    删除nuxt.config.ts中多余的public目录配置
    更新部署工作流中不再需要特定base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,10 +9,6 @@ export default defineNuxtConfig({
     },
   },
 
-  dir: {
-    public: "/ChatbotPage/public",
-  },
-
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@element-plus/nuxt"],
